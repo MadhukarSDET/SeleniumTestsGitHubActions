c601cc14c302bfc911bf1c7d665c0c196e40f59a
Madhukar <mmadhukar.qa@gmail.com>
2025-10-30 02:32:42 -0700

    Update Test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,25 +3,46 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-	
+
 	protected WebDriver driver;
 
 	@BeforeMethod
 	public void setupBrowser() {
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
-		// options.addArguments("--headless=new");
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--incognito");
-		driver = new ChromeDriver(options);
+
+		String browser = System.getProperty("browser", "chrome");
+
+		switch (browser.toLowerCase()) {
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			break;
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			break;
+		default:
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--incognito");
+			driver = new ChromeDriver(options);
+			break;
+		}
 		driver.get("https://automationexercise.com/");
+		driver.manage().window().maximize();
 	}
 
 	@AfterMethod
